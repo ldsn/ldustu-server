87375e6c4b5b102e7cc6b7468162f74cb5cc532f
arthurjason <351192873@qq.com>
2015-02-07 22:30:30 +0800

    push something

--- server/Application/Home/Controller/IndexController.class.php
+++ server/Application/Home/Controller/IndexController.class.php
@@ -6,6 +6,12 @@ class IndexController extends Controller {
        public function index(){//测试页面
               $user = D('User');
               $username = cookie('username');
+              echo '这是首页';
 
+          }
+          public function getArticle(){ //获取文章页面
+            $article =D('Article');
+            $result = $article->relation(true)->select();
+            dump($result);
           }
 }
