1d6fe99ffd93e1a8c121b1ad1e122b60f8ad3780
arthurjason <351192873@qq.com>
2015-02-08 16:20:15 +0800

    functions of index

--- server/Application/Home/Controller/IndexController.class.php
+++ server/Application/Home/Controller/IndexController.class.php
@@ -4,14 +4,15 @@ use Think\Controller;
 header('Content-Type: text/html; charset=utf-8;');
 class IndexController extends Controller {
        public function index(){//测试页面
-              $user = D('User');
-              $username = cookie('username');
               echo '这是首页';
+              $user = D('User');
+              $username =jason;
+              print_r($user->userinfo($username));
+             $article = D('article');
+              print_r($article->getArticle(0));
 
           }
-          public function getArticle(){ //获取文章页面
-            $article =D('Article');
-            $result = $article->relation(true)->select();
-            dump($result);
-          }
+          
+
+
 }
---
+++ server/Application/Home/Model/ArticleModel.class.php
@@ -0,0 +1,28 @@
+<?php
+namespace Home\Model;
+use Think\Model\RelationModel;
+class ArticleModel extends RelationModel{
+	protected $_link = array(
+		'Article_detial' =>array(
+				'mapping_type'=>self::HAS_ONE,
+				'mapping_name'=>'Article_detial',
+				'class_name'=>'Article_detial',
+				'foreign_key'=>'art_id',
+				
+			),
+		);
+	public function getArticle($getPage){ //获取文章页面
+            $num = $getPage*10;
+            $result = $this->limit($num,20)->where('ismake=1')->order('art_time desc')->select();
+            $Output = array(
+                      'error'=>0,
+                      'data' =>array(
+                              $result
+                        ),
+                );	
+
+           return  $result1 = json_encode($Output);
+           
+        	}	
+        	public function 
+} 
