c9955767c90c332a92d34cc056106aab290e9d8b
arthurjason <351192873@qq.com>
2015-02-11 22:04:20 +0800

    调整了接口

--- server/Application/Home/Model/ArticleModel.class.php
+++ server/Application/Home/Model/ArticleModel.class.php
@@ -8,25 +8,46 @@ class ArticleModel extends RelationModel{
 				'mapping_type'=>self::HAS_ONE,
 				'mapping_name'=>'Article_detial',
 				'class_name'=>'Article_detial',
-				'foreign_key'=>'art_id',
-				
+				'foreign_key'=>'art_id',		
 			),
 		);
 	public function getArticle($startid,$getnum,$cid){ //获取文章页面
+            $user = M('user');
+            $error = 0;
             $where = array(
                 'ismake' => 1,
                 'clu_id' =>$cid,
                );
-            $result = $this->limit($startid,$getnum)->where($where)->order('art_time desc')->select();
+            $result =  $this->limit($startid,$getnum)->where($where)->order('time desc')->select();
             $listNum = count($result);
             if($listNum<$getnum){
-              $end = false;
-            }else{
               $end = true;
+            }else{
+              $end = false;
+            }
+            foreach($result as $key=> $value){    
+                       $new[$key]['art_id'] = (int)$value['art_id'];
+                       $new[$key]['user_id'] = (int)$value['user_id'];
+                       $new[$key]['clu_id'] = (int)$value['clu_id'];
+                       $new[$key]['ismake'] = (int)$value['ismake'];
+                       $new[$key]['favour'] = (int)$value['favour'];
+                       $new[$key]['visit'] = (int)$value['visit']; 
+                       $new[$key]['comment'] = (int)$value['comment']; 
+                       $new[$key]['title'] = $value['title'];
+                       $new[$key]['desription'] = $value['desription'];
+                       $new[$key]['time'] = (int)$value['time'];
+                       $new[$key]['from'] = $value['from'];
+                       $where['user_id'] = $new[$key]['user_id'];
+                       $userinfo = $user->where($where)->field('username')->find();
+                       $new[$key]['username'] = $userinfo['username'] ;
+            }
+            if(!$result||$result ==''){
+               $error = 1;
+               $new = '取不到数据';
             }
             $Output = array(
-                      'error'=>0,
-                      'data' =>$result,
+                      'error'=>$error,
+                      'data' =>$new,
                       'end' =>$end,
                       'listNum' =>$listNum,
                 );	
@@ -34,8 +55,11 @@ class ArticleModel extends RelationModel{
            return  $result1 = json_encode($Output);
            
         	}	
-        	public function favour($art_id){
-        		$where['art_id'] = $art_id;
+        	public function favour($art_id,$user_id){
+        		$where = array(
+                            'art_id' => $art_id,
+                            'user_id' => $user_id,
+                          );
         		$this->where($where)->select();
         	}
             
