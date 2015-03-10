59fcc978d23aa15a267981e0849090b86e0d6ebc
edire <az8641683@163.com>
2015-03-11 00:38:16 +0800

    配置模板信息

--- server/Application/Wap/Conf/config.php
+++ server/Application/Wap/Conf/config.php
@@ -3,4 +3,13 @@ return array(
 	//'配置项'=>'配置值'
 	'name' =>'jason',
 //	'URL_MODEL'=>0,
+    'TMPL_ENGINE_TYPE'=>'Smarty',
+    'TMPL_TEMPLATE_SUFFIX'=>'.tpl',
+    'TMPL_ENGINE_CONFIG'=>array(
+        'plugins_dir'=>'./Application/Wap/Plugins/',
+        'template_dir'=>'./Application/Wap/Temp/',
+        'config_dir'=>'./Application/Wap/Config/',
+        'left_delimiter'=>'{%',
+        'right_delimiter'=>'%}'
+    ),
 );
