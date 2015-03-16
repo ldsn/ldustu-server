bc9137dae60c6fbf920e256b3bbd585940abf7d2
ETY001 <ety001@domyself.me>
2015-03-17 02:16:33 +0800

    用户模块

--- server/Application/Wap/Model/UserModel.class.php
+++ server/Application/Wap/Model/UserModel.class.php
@@ -6,7 +6,7 @@ class UserModel extends Model{
         array('verify','require','need_auth_code'), //验证码
         array('password','require','need_password'), //密码
         array('password','8,16','pass_not_long',1, 'length'), //密码
-        //array('verify','check_verify','auth_code_err', 1, 'function' ), //验证码
+        array('verify','check_verify','auth_code_err', 1, 'function' ), //验证码
         array('username','','username_has_existed',1,'unique',1), // 在新增的时候验证username字段是否唯一
         array('email','email','email_err',1), // 邮箱格式
         array('email','','email_has_existed',1,'unique',1), // 在新增的时候验证email字段是否唯一
