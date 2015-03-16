15b86d27ec91d806b9b53add331deef9dc54cf15
ETY001 <ety001@domyself.me>
2015-03-16 19:41:49 +0800

    catchComment

--- server/Application/Wap/Model/CommentModel.class.php
+++ server/Application/Wap/Model/CommentModel.class.php
@@ -2,78 +2,61 @@
 namespace Wap\Model;
 use Think\Model;
 class CommentModel extends Model{
-	public function catchComment($aid,$startid = 0,$count = 20){
-		if(!isset($aid)||!isset($count)){
-			$returnJson=array(
-			'error'=>1001,
-			);
-		}else{	
-			if($count>50){
-				$count = 50;
-				$returnJson['error'] = 1010;
-			}
-				$where['aid'] = (int)$aid;
-				$result =$this->limit($startid,$count)->where($where)->order('time desc')->select();
-				if($result&&$result!=''){
-					$returnJson = array(
-						'error'=>0,
-					); 
-				}else{
-					$returnJson = array(
-						'error'=>1002,
-					);
-				}
-			
-		}
-		$result['error'] = $returnJson['error'];
-		return $result;
-	}
-	public function comment($uid,$aid,$content ){	//评论动作
-		if( !isset($uid)||!isset($aid)||!isset($content) ){
-			$returnJson=array(
-			'error'=>1001,
-			);
-		}else{
-			$time = time();
-			$data = array(
-				'uid' => $uid,
-				'aid' =>$aid,
-				'content' =>$content,
-				'time' =>$time,
-				);
-			$result = $this->add($data);
-			if($result&&$result!=''){
-				$returnJson=array(
-				'error'=>0,
-				);
-			}else{
-				$returnJson=array(
-				'error'=>1002,
-				);
-			}
-		}			
-		return $returnJson;
-		
-	}
-	public function deleteComment($com_id){ //删除留言
-		if(!isset($com_id)){
-			$returnJson=array(
-			'error'=>1001,
-			);
-		}else{
-			$where['id'] = $com_id;
-			$result = $this ->where($where)->delete();
-			if($result&&$result!=''){
-				$returnJson=array(
-				'error'=>0,
-				);
-			}else{
-				$returnJson=array(
-				'error'=>1002,
-				);
-			}
-		}
-		
-		return $returnJson;
-	}
+    public function catchComment($conditions=array(), $startid=0, $count=20, $order='id desc'){
+        if(!$conditions)return false;
+        $result     = $this->limit($startid, $count)
+                            ->where($conditions)
+                            ->order($order)
+                            ->select();
+        return $result;
+    }
+
+    public function comment($uid,$aid,$content ){   //评论动作
+        if( !isset($uid)||!isset($aid)||!isset($content) ){
+            $returnJson=array(
+            'error'=>1001,
+            );
+        }else{
+            $time = time();
+            $data = array(
+                'uid' => $uid,
+                'aid' =>$aid,
+                'content' =>$content,
+                'time' =>$time,
+                );
+            $result = $this->add($data);
+            if($result&&$result!=''){
+                $returnJson=array(
+                'error'=>0,
+                );
+            }else{
+                $returnJson=array(
+                'error'=>1002,
+                );
+            }
+        }           
+        return $returnJson;
+        
+    }
+    public function deleteComment($com_id){ //删除留言
+        if(!isset($com_id)){
+            $returnJson=array(
+            'error'=>1001,
+            );
+        }else{
+            $where['id'] = $com_id;
+            $result = $this ->where($where)->delete();
+            if($result&&$result!=''){
+                $returnJson=array(
+                'error'=>0,
+                );
+            }else{
+                $returnJson=array(
+                'error'=>1002,
+                );
+            }
+        }
+        
+        return $returnJson;
+    }
 }
