bd8ce5b66494975b21bb813b5975606b7d5c3e4c
edire <az8641683@163.com>
2015-05-25 20:51:31 +0800

    修改rewrite配置 添加 conf文件

--- server/Application/Common/Conf/config.php
+++ server/Application/Common/Conf/config.php
@@ -19,7 +19,8 @@ return array(
     //路由设置
     'URL_ROUTER_ON'   => true,          //开启路由
     'URL_ROUTE_RULES' => array( //定义路由规则
-    'listArticle' => 'index/listArticle'
+        'listArticle' => 'index/listArticle',
+        'articleArticle' => 'index/articleArticle'
      ),
     'URL_MODEL'             =>1,  //url模式  pathinfo
     'URL_CASE_INSENSITIVE'  => true, //URL不区分大小写
