9356ed4be0cf0ece96988e0cbdff56a40bd97db6
arthurjason <351192873@qq.com>
2015-06-03 17:58:37 +0800

    增加了内容页面取出更新信息

--- server/Application/Common/Model/ArticleModel.class.php
+++ server/Application/Common/Model/ArticleModel.class.php
@@ -47,13 +47,11 @@ class ArticleModel extends RelationModel{
                           ->find();
         $update         = M('Article_update');
         $user           = M('User');
-        $tb_update      = $update->where('article_id='.$aid)->select();
-        // foreach($tb_update as $k => $v)
-        // {
-        //   $tb_update[$k]['user_id'] = $user->where('user_id='.$tb_update[$k]['user_id'])->field('username')->find();
-        // }
-        var_dump($tb_update);
-        $up_username    = $user->where('user_id='.$tb_update['user_id'])->field('username')->find();
+        $tb_update      = $update->where($conditions)->select();
+        foreach($tb_update as $k => $v)
+        {
+          $tb_update[$k]['user_id'] = $user->where('user_id='.$tb_update[$k]['user_id'])->field('username')->find();
+        }
         $this->where($conditions)->setInc('view_num',1);
         if($result){
             $comment_model                  = D('Comment');
