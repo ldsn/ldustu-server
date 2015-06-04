a98adcdb9a5d75212abea7d545a7e58867fa808f
arthurjason <351192873@qq.com>
2015-06-04 15:08:23 +0800

    测试用户个人中心

--- server/Application/Pc/Controller/UserController.class.php
+++ server/Application/Pc/Controller/UserController.class.php
@@ -3,6 +3,10 @@ namespace Pc\Controller;
 use Think\Controller;
 header('Content-Type: text/html; charset=utf-8;');
 class UserController extends Controller{
+	/**
+	 * 用户后台首页，提取用户信息
+	 * @author Jason
+	 */
 	public function index()
 	{	
 		$user     = D('User');
@@ -10,5 +14,12 @@ class UserController extends Controller{
 		$userinfo = $user->userinfo($user_id);
 		var_dump($userinfo);
 		$this->display();
+	}
+	/**
+	 * 用户个人中心
+	 */
+	public function info_center()
+	{
+
 	}
 }
