53fe62072eb1f7e55f0590dedd5e243fb001472a
arthurjason <351192873@qq.com>
2015-06-08 18:49:06 +0800

    修正了用户中心的错误

--- server/Application/Pc/Controller/UserController.class.php
+++ server/Application/Pc/Controller/UserController.class.php
@@ -11,40 +11,40 @@ class UserController extends Controller{
 	{	
 		//提取用户信息
 		$user     = D('User');
-		$user_id  = session('user_info.user_id')?session('user_info.user_id'):0;
+		$user_id  = I('get.user_id')?I('get.user_id'):0;
 		//判断权限
-		$usercheck= $user->where('user_id='.$user_id)->find();
-		if($usercheck){
+		if($user_id == session('user_info.user_id')){
 			$is_me = 'is_me';
 			$this->assign('is_me',$is_me);
+			//提取评论你
+			$comment  = M('Comment');
+			$com_info = $comment->where('user_id='.$user_id)->select();
+			foreach ($com_info as $key => $value) {
+				$article_com 				  = $article->where('article_id='.$com_info[$key]['article_id'])->find();
+				$com_info[$key]['article_title'] = $article_com['title'];
+			}
+			$this->assign('com_info',$com_info);
 		}
 		$user_info= $user->userinfo($user_id);
 		//提取用户发表文章
 		$article  = M('Article');
 		$art_info = $article->where('user_id='.$user_id)->select();
-		//提取评论你
-		$comment  = M('Comment');
-		$com_info = $comment->where('user_id='.$user_id)->select();
-		foreach ($com_info as $key => $value) {
-			$article_com 				  = $article->where('article_id='.$com_info[$key]['article_id'])->find();
-			$com_info[$key]['article_title'] = $article_com['title'];
-		}
+
 		//信息打到模版变量
-		$this->assign('com_info',$com_info);
 		$this->assign('art_info',$art_info);
 		$this->assign('user_info',$user_info);
 		$this->display();
 	}
 	/**
 	 * 用户个人信息
+	 * @author Jason
 	 */
 	public function info_center()
 	{
 		$user     = D('User');
-		$user_id  = session('user_info.user_id')?session('user_info.user_id'):0;
+		$user_id  = I('get.user_id')?I('get.user_id'):0;
 		//判断权限
-		$usercheck= $user->where('user_id='.$user_id)->find();
-		if($usercheck){
+		if($user_id == session('user_info.user_id')){
 			$is_me = 'is_me';
 			$this->assign('is_me',$is_me);
 		}
