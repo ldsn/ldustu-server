09a5fcdeb267fc18c0388ff716e16c8b6e6994c5
edire <az8641683@163.com>
2015-06-08 20:35:11 +0800

    个人页面

--- server/Application/Pc/Controller/UserController.class.php
+++ server/Application/Pc/Controller/UserController.class.php
@@ -22,17 +22,19 @@ class UserController extends Controller{
 		$comment  = D('Comment');
 		$com_info = $comment->where(array('user_id'=>$user_id))->getList();
 		foreach ($com_info as $key => $value) {
-			$article_com 				  = $article->where(array('article_id'=>$com_info[$key]['article_id']))->getList();
-			$com_info[$key]['article_title'] = $article_com['title'];
+			$article_com 				  = $article->where(array('article_id'=>$com_info[$key]['article_id']))->select();
+			$com_info[$key]['article_title'] = $article_com[0]['title'];
 		}
-		$this->assign('com_info',$com_info);
-		$user_info= $user->userinfo($user_id);
+		$home_info= $user->userinfo($user_id);
+		$user_info= $user->userinfo(session('user_info.user_id'));
 		//提取用户发表文章
 		
 		$art_info = $article->where(array('user_id'=>$user_id))->getList();
 
 		//信息打到模版变量
+		$this->assign('com_info',$com_info);
 		$this->assign('art_info',$art_info);
+		$this->assign('home_info',$home_info);
 		$this->assign('user_info',$user_info);
 		$this->display('ldsn-pc/page/home');
 	}
@@ -46,11 +48,14 @@ class UserController extends Controller{
 		$user_id  = I('get.user_id')?I('get.user_id'):0;
 		//判断权限
 		if($user_id == session('user_info.user_id')){
-			$is_me = 'is_me';
+			$is_me = true;
 			$this->assign('is_me',$is_me);
 		}
-		$user_info = $user->userinfo($user_id);
+		$home_info = $user->userinfo($user_id);
+		$user_info = $user->userinfo(session('user_info.user_id'));
+
 		//用户信息打到模版变量
+		$this->assign('home_info',$home_info);
 		$this->assign('user_info',$user_info);
 		$this->display('ldsn-pc/page/info');
 	}
